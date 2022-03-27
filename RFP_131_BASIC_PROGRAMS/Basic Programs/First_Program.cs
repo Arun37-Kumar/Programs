f8e482db565a37fc271e6e6f4e638209517e772b
Arun37-Kumar <arun007624@gmail.com>
2022-03-27 19:18:42 +0530

    files added

---
+++ RFP_131_BASIC_PROGRAMS/Basic Programs/First_Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_131_BASIC_PROGRAMS.Basic_Programs
+{
+    class First_Program
+    {
+        public static void FirstProgram()
+        {
+            Console.WriteLine("This is the first Program");
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ RFP_131_BASIC_PROGRAMS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_131_BASIC_PROGRAMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Basic_Programs.First_Program.FirstProgram();
+        }
+    }
+}
