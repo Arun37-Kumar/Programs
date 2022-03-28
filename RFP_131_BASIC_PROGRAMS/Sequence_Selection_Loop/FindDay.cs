3de67cc99f30c245fb1f3a6b90e912b6331fce28
Arun37-Kumar <arun007624@gmail.com>
2022-03-28 09:44:38 +0530

    commit change

---
+++ RFP_131_BASIC_PROGRAMS/Sequence_Selection_Loop/FindDay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_131_BASIC_PROGRAMS.Sequence_Selection_Loop
+{
+    class FindDay
+    {
+        public static void Week() 
+        { 
+        int weekDayNumber;
+            Console.WriteLine("Enter the week Day : ");
+            weekDayNumber = Convert.ToInt32(Console.ReadLine());
+            switch (weekDayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Number!");
+                    break;
+            }
+        }
+    }
+}
